4ea1e0084fc97748429314d46077aeaac1281ad0
deshpandesoniya16 <deshpandesoniya16@gmail.com>
2019-04-07 11:21:36 +0530

    Upated the css
    1)index.css
    2)header.css
    3)post.css
    4)bloglist.css

--- js/index.js
+++ js/index.js
@@ -1,26 +1,27 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+var postModal = document.getElementById('create-post-modal');
 
 // Get the button that opens the modal
 var createPost = document.getElementById("create-post");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("postModal-close")[0];
 
 // When the user clicks the button, open the modal
 createPost.onclick = function() {
-    modal.style.display = "block";
+    postModal.style.display = "block";
 
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+    postModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == postModal) {
+        postModal.style.display = "block";
     }
+	
 }
